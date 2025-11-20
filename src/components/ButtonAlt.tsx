921c56b799a8ff1c42914431e63bf495ea0e2e56
DeepBrain07 <olusatimmie07@gmail.com>
2025-11-20 05:25:10 +0100

    Building Landing Page

--- src/components/ButtonAlt.tsx
+++ src/components/ButtonAlt.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 export const Button = ({title, className, icon}:ButtonProps) => {
    return (
     <div
-      className={`text-center h-fit cursor-pointer border-[0.5px] hover:bg-primary/10 border-primary/10 flex justify-center items-center gap-2 text-xs sm:text-sm ${className}  p-6 px-4`}
+      className={`text-center h-fit cursor-pointer border-[0.5px] hover:bg-primary/10 hover:text-white border-primary/10 flex justify-center items-center gap-2 text-xs sm:text-sm ${className}  p-6 px-4`}
     >
       {icon && <Icon icon={icon} width={20} className=" cursor-pointer"/>}
       <p className={` sm:block`}>{title}</p>
